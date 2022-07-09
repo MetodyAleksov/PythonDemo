956589a9976c8e01896d49e9e7d9461532019bbb
Metody Aleksov <teddy57603@gmail.com>
2022-07-09 18:53:09 +0300

    tested for loops

--- demo.py
+++ demo.py
@@ -1,25 +1,4 @@
-import random
+arr = ["Left", "Right", "Center"]
 
-upperCeil = 101
-lowerCeil = 0
-
-computerAttempts = 0
-
-while(True):
-    computerNumber = random.randint(lowerCeil, upperCeil)
-    print(computerNumber)
-    computerAttempts += 1
-
-    userInput = input("Is the number correct? Y/N: ")
-
-    if(userInput.upper() == "Y"):
-        break;
-    else:
-        userInput = input("Is the number generated greater or smaller? G/S: ")
-
-        if userInput.upper() == "G":
-            lowerCeil = computerNumber + 1
-        else:
-            upperCeil = computerNumber
-
-print("The computer guessed the number in", computerAttempts, "attempts")
+for i in range(len(arr)):
+    print(arr[i])
