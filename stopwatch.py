73f4a8696325ef0a06d884c34521e33c308dbde0
Metody Aleksov <teddy57603@gmail.com>
2022-07-31 19:15:32 +0300

    dropped

--- stopwatch.py
+++ stopwatch.py
@@ -1,5 +1,18 @@
+from calendar import c
+
+
 class Chronometer:
-    def __init__(self, time):
-        self.time = time
-        
+    time = 0
     
+    def __init__(self) -> None:
+        pass
+        
+c1 = Chronometer()
+c2 = Chronometer()
+
+c1.time = 10
+c2.time = 200
+
+print(Chronometer.time)
+print(c1.time)
+print(c2.time)
